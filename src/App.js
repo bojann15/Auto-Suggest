99b3cbc2958eb0247cd28e7ed3428fc93f3d5f6c
bojann15 <krsmanovic3@gmail.com>
2021-11-03 21:38:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import example from './data/example.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [text, setText] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
 
+    const handler = (text) => {
+        let matches = [];
+        if (text.length > 0) {
+            matches = example.filter((item) => {
+                const regex = new RegExp(`^${text}`, 'gi')
+                return item.name.match(regex);
+            })
+        }
+        setTimeout(() => {
+            setSuggestions(matches);
+        }, 700);
+        setText(text);
+    };
+
+    const suggestionHandler = (text) => {
+        setText(text);
+        setSuggestions([]);
+    };
+
+    return (
+        <div className="App">
+            <input className="input" type="text" value={text} onChange={(e) => handler(e.target.value)} onBlur={() => {
+                setTimeout(() => {
+                    setSuggestions([])
+                }, 100);
+            }} />
+            {suggestions?.map((item, index) => {
+                return (
+                    <div key={index} className="suggestion" onClick={() => suggestionHandler(item.name)}>{item.name}</div>
+                )
+            })}
+        </div >
+    )
+};
 export default App;
